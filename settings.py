7e94add93f9a76994db4daec80affe1c66cbb348
Ivan Rychkov <rychkov.ivan@gmail.com>
2023-09-11 23:58:35 +0300

    progress towards redemption

--- settings.py
+++ settings.py
@@ -1,2 +1,2 @@
-USER_HOST = 'xx.xx.xx.xx' # укажите свой хост
-TOPIC_NAME = 'student.topic.cohort<номер когорты>.<username>' # укажите название топика student.topic.cohort<номер когорты>.<username>
+USER_HOST = '158.160.114.14'  # укажите свой хост
+TOPIC_NAME = 'student.topic.cohort14.ivanrychkov'  # укажите название топика student.topic.cohort<номер когорты>.<username>
