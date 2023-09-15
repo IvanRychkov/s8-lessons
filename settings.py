16e96d37791efe1349db7fec16cc809ede4ea232
Ivan Rychkov <rychkov.ivan@gmail.com>
2023-09-16 00:21:36 +0300

    postgres done

--- settings.py
+++ settings.py
@@ -1,2 +1,2 @@
-USER_HOST = '84.201.130.1'  # укажите свой хост
+USER_HOST = '51.250.78.217'  # укажите свой хост
 TOPIC_NAME = 'student.topic.cohort14.ivanrychkov'  # укажите название топика student.topic.cohort<номер когорты>.<username>
--- Тема 3. Настройка потока данных/7. Подключение к Postres и изучение данных/Задание 1/realization.py
+++ Тема 3. Настройка потока данных/7. Подключение к Postres и изучение данных/Задание 1/realization.py
@@ -1,10 +1,24 @@
 from pyspark.sql import SparkSession
 
 spark = (
-        #
-    )
+    SparkSession
+    .builder
+    .appName('spark reads from postgres')
+    .config('spark.jars.packages', 'org.postgresql:postgresql:42.4.0')
+    .getOrCreate()
+)
 
-df = (spark.read
-        #
-        .load())
+df = (
+    spark
+    .read.jdbc(
+        url='jdbc:postgresql://rc1a-fswjkpli01zafgjm.mdb.yandexcloud.net:6432/de',
+        table='public.marketing_companies',
+        properties=dict(
+            driver='org.postgresql.Driver',
+            user='student',
+            password='de-student',
+        )
+    )
+)
+df.printSchema()
 df.count()
